52f2db37d7ab3103492c5afc132d6cac6ee7f8ea
Maks Fisli <mfisli2@gmail.com>
2017-07-29 13:45:09 -0700

    Added staff search function (exact name match for now. Also modified the building of staff html to dyamically show employees if sreached for - else it shows all staff.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 // TODO:
-// Get users displayed on about page
-// Search function for contact us page
-// Apply pagination to posts on home page
+// Responsive layout on contract us
+// Search function on contract us
+// Pagination on posts on home page
 
 'use strict';
+var staff = {
+    listings : []
+};
 function log(msg,obj){
     console.log(msg); 
     if(obj){
@@ -48,42 +51,46 @@ function getPosts() {
     xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
     xhttp.send();
 }
-function buildStaffHTML(obj){
-    if(obj){
-        log("Building HTML with " + obj.length + " staff(s)."); 
+function buildStaffHTML(staffMembers){
+    var staffHTML = document.getElementById("staff");
+    staffHTML.innerHTML = ""; 
+    if(!staffMembers){
+        var staffMembers = staff.listings;
+    }
+    if(staffMembers.length != 0){
+        log("Building HTML with " + staffMembers.length + " employees(s)."); 
     } else {
         log("ERROR! Staff is empty.");
         return;
     }
     // TODO replace hard-coded keys 
-    var staffHTML = document.getElementById("staff");
-    staffHTML.innerHTML = ""; 
-    for(var i = 0; i < obj.length; i++){
+
+    for(var i = 0; i < staffMembers.length; i++){
         staffHTML.innerHTML += 
         "<div class='employee'>" +
-         "<img src='http://via.placeholder.com/150x150' alt=\'" + obj[i].name + " photo\'/>" + 
-         "<h4 class='employee-name'>" + obj[i].name + "<br /></h4>" +  
-         "<p class='employee-email'> Email: " +  obj[i].email + "</p>" +
-         "<p class='employee-phone'> Phone: " +  obj[i].phone + "</p>" +
-         "<p class='employee-catch-phrase'>" + obj[i].company.catchPhrase + "</p>" + 
+         "<img src='http://via.placeholder.com/150x150' alt=\'" + staffMembers[i].name + " photo\'/>" + 
+         "<h4 class='employee-name'>" + staffMembers[i].name + "<br /></h4>" +  
+         "<p class='employee-email'> Email: " +  staffMembers[i].email + "</p>" +
+         "<p class='employee-phone'> Phone: " +  staffMembers[i].phone + "</p>" +
+         "<p class='employee-catch-phrase'>" + staffMembers[i].company.catchPhrase + "</p>" + 
         "</div>"; 
     }
 
 }
 function getEmployees(){
-    var staff = document.getElementById("staff");
-    if (!staff){
+    var staffHTML = document.getElementById("staff");
+    if (!staffHTML){
         log("ERROR! No staff element.");
         return;
     }
     log("Getting employees ...");
-    staff.innerHTML = "Loading Staff ...";
+    staffHTML.innerHTML = "Loading Staff ...";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             log("Got employees");
-            let jsonObj = JSON.parse(this.responseText);
-            buildStaffHTML(jsonObj);
+            staff.listings = JSON.parse(this.responseText);
+            buildStaffHTML();
         }
     }
     xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
@@ -91,7 +98,7 @@ function getEmployees(){
 } 
 function buildAboutUsHTML(obj){
     if(!obj){
-        log("ERROR! Staff is empty.");
+        log("ERROR! About us is empty.");
         return;
     }
     log("Building About Us HTML"); 
@@ -125,35 +132,25 @@ function getAboutUs(){
     }
     xhttp.open("Get", "https://baconipsum.com/api/?type=meat-and-filler&paras=3", true);
     xhttp.send(); 
-
 }
-
-/*
-function getUsers() { // this would be cleaner with jQuery when().then()
-    log("Getting Users ...");
-    document.getElementById("blog").innerHTML = "Loading Users ...";
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            log("Got users.");
-            let rawPayload = this.responseText;
-            let jsonObj = JSON.parse(rawPayload);
-            usersArray = processUsers(jsonObj);   // don't really need a constr; it's already an object
-            usersReady = true;
-        }
-    };
-    xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
-    xhttp.send();
-}
-
-function processUsers(obj){
-    var blogArray = []; 
-    for( var i = 0; i < obj.length; i++){
-        let name = obj[i].username;
-        let city = obj[i].address.city;
-        let subject = obj[i].company.catchPhrase;
-        let body = obj[i].company.bs;
-        blogArray.push(new Blog(name,city,subject,body));
+function searchNames(){
+    var target = document.getElementById('name').value; 
+    if (!target){
+        log("No names given.");
+        buildStaffHTML(staff.listings);
+        return;
     }
-    return blogArray;
-}*/
+    if(!staff.listings.length){
+        log('Nothing to search');
+        return;
+    }
+    log("Considering " + staff.listings.length + " Staff member(s).");
+    var result = []; 
+    for(let employee of staff.listings){
+        if (employee.name == target){
+            result.push(employee);
+            log("Added " + employee.name);
+        }
+    }
+    buildStaffHTML(result);
+}
